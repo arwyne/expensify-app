8c4d52684c94c73a3b9946155020317e475b778d
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-09-28 19:08:00 +0800

    Set production build and server

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
 import "normalize.css/normalize.css";
 import "./styles/style.scss";
+import "react-dates/lib/css/_datepicker.css";
+import "react-dates/initialize";
 
 // redux
 import { Provider } from "react-redux";
